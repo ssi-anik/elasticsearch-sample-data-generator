db845c04a80b762412fde349a016aa77c9fefd80
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-03-22 23:53:06 +0600

    command update with file opening and option filtering

--- app/Commands/GenerateCommand.php
+++ app/Commands/GenerateCommand.php
@@ -4,9 +4,12 @@ namespace App\Commands;
 
 use Faker\Generator;
 use LaravelZero\Framework\Commands\Command;
+use Throwable;
 
 class GenerateCommand extends Command
 {
+    private $filePointer;
+
     protected $signature = 'generate 
                             {fields : Enter the fields definition (required)}
                             {--file=dump.json : Enter the file name}
@@ -14,6 +17,7 @@ class GenerateCommand extends Command
                             {--action=index : Enter the action name [index or create]}
                             {--index=my-es-index : Enter the index name}
                             {--id=1 : Enter the sequence start value}
+                            {--append : Append to existing file}
                             ';
 
     protected $description = 'Generate dump for elasticsearch bulk API upload';
@@ -22,11 +26,15 @@ class GenerateCommand extends Command
     {
         $fields = $this->argument('fields');
         $file = $this->option('file');
-        $entries = $this->option('entries');
+        $entries = $this->getNoOfEntries();
         $action = $this->option('action');
         $index = $this->option('index');
-        $docStartId = $this->option('id');
+        $docStartId = $this->documentStartId();
+        $mode = $this->option('append') ? 'a' : 'w';
 
+        /**
+         * Ask user for confirmation
+         */
         $this->info('The following values will be considered.');
         $this->table(['For the', 'will use'], [
             ['File', $file],
@@ -34,12 +42,56 @@ class GenerateCommand extends Command
             ['Action type', $action],
             ['Index', $index],
             ['Document ID starts from', $docStartId],
+            ['Append if file exists', $mode === 'a' ? 'True' : 'False'],
         ]);
+
         /*if (false === $this->confirm('Proceed?', true)) {
             $this->warn('Exiting...');
 
             return;
         }*/
-        dd($faker->custom('designation'));
+        /**
+         * User confirmed
+         */
+        if (false === $this->canCreateFile($file, $mode)) {
+            $this->output->error('Cannot create file on ' . $file);
+
+            return;
+        }
+
+        $this->closeFile();
+    }
+
+    private function writeToFile(string $line)
+    {
+        fwrite($this->filePointer, $line);
+    }
+
+    private function closeFile()
+    {
+        fclose($this->filePointer);
+    }
+
+    private function canCreateFile($file, $mode = 'w'): bool
+    {
+        try {
+            return !!($this->filePointer = fopen($file, $mode));
+        } catch (Throwable $t) {
+            return false;
+        }
+    }
+
+    private function getNoOfEntries(): int
+    {
+        $noOfEntries = (int)$this->option('entries');
+
+        return $noOfEntries > 0 ? $noOfEntries : 1;
+    }
+
+    private function documentStartId(): int
+    {
+        $docStartID = (int)$this->option('id');
+
+        return $docStartID >= 0 ? $docStartID : 1;
     }
 }
