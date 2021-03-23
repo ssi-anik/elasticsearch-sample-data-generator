ae93d8b09e47321248a380b9a5543bf2ea9bbf0a
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-03-23 09:23:08 +0600

    command can generate data and writes to stdout

--- app/Commands/GenerateCommand.php
+++ app/Commands/GenerateCommand.php
@@ -9,6 +9,14 @@ use Throwable;
 class GenerateCommand extends Command
 {
     private $filePointer;
+    private $fields;
+    private $file;
+    private $entries;
+    private $action;
+    private $index;
+    private $docStartId;
+    private $mode;
+    private $generator;
 
     protected $signature = 'generate 
                             {fields : Enter the fields definition (required)}
@@ -22,44 +30,69 @@ class GenerateCommand extends Command
 
     protected $description = 'Generate dump for elasticsearch bulk API upload';
 
+    public function __construct(Generator $generator)
+    {
+        parent::__construct();
+        $this->generator = $generator;
+    }
+
+    public function __destruct()
+    {
+        $this->closeFile();
+    }
+
     public function handle(Generator $faker)
     {
-        $fields = $this->argument('fields');
-        $file = $this->option('file');
-        $entries = $this->getNoOfEntries();
-        $action = $this->option('action');
-        $index = $this->option('index');
-        $docStartId = $this->documentStartId();
-        $mode = $this->option('append') ? 'a' : 'w';
-
-        /**
-         * Ask user for confirmation
-         */
-        $this->info('The following values will be considered.');
-        $this->table(['For the', 'will use'], [
-            ['File', $file],
-            ['No of entries', $entries],
-            ['Action type', $action],
-            ['Index', $index],
-            ['Document ID starts from', $docStartId],
-            ['Append if file exists', $mode === 'a' ? 'True' : 'False'],
-        ]);
-
-        /*if (false === $this->confirm('Proceed?', true)) {
+        $this->prepareOptions();
+        if (false === $this->askForConfirmation()) {
             $this->warn('Exiting...');
-
-            return;
-        }*/
-        /**
-         * User confirmed
-         */
-        if (false === $this->canCreateFile($file, $mode)) {
-            $this->output->error('Cannot create file on ' . $file);
 
             return;
         }
 
-        $this->closeFile();
+        if (false === $this->canCreateFile()) {
+            return;
+        }
+
+        $this->generateAndWriteDump();
+    }
+
+    private function prepareOptions(): void
+    {
+        $this->fields = $this->argument('fields');
+        $this->file = $this->option('file');
+        $this->entries = $this->getNoOfEntries();
+        $this->action = $this->option('action');
+        $this->index = $this->option('index');
+        $this->docStartId = $this->documentStartId();
+        $this->mode = $this->option('append') ? 'a' : 'w';
+    }
+
+    private function askForConfirmation(): bool
+    {
+        $this->info('The following values will be considered.');
+        $this->table(['Options', 'Will use'], [
+            ['File', $this->file],
+            ['No of entries', $this->entries],
+            ['Action type', $this->action],
+            ['Index', $this->index],
+            ['Document ID starts from', $this->docStartId],
+            ['Append if file exists', $this->mode === 'a' ? 'True' : 'False'],
+        ]);
+
+        // return $this->confirm('Proceed?', true);
+        return true;
+    }
+
+    private function canCreateFile(): bool
+    {
+        try {
+            return !!($this->filePointer = fopen($this->file, $this->mode));
+        } catch (Throwable $t) {
+            $this->output->error('Cannot create file on ' . $this->file);
+
+            return false;
+        }
     }
 
     private function writeToFile(string $line)
@@ -69,16 +102,57 @@ class GenerateCommand extends Command
 
     private function closeFile()
     {
-        fclose($this->filePointer);
+        $this->filePointer ? fclose($this->filePointer) : null;
     }
 
-    private function canCreateFile($file, $mode = 'w'): bool
+    private function generateAndWriteDump()
     {
-        try {
-            return !!($this->filePointer = fopen($file, $mode));
-        } catch (Throwable $t) {
-            return false;
+        $fields = $this->parseFields();
+        foreach (range(1, $this->entries) as $entry) {
+            $rows = [];
+            foreach ($fields as $field) {
+                [$path, $method, $parameters] = $this->pathAndFieldResolver($field);
+                $value = call_user_func_array([$this->generator, $method], $parameters);
+                $rows = array_merge_recursive($rows, $this->dotNotationToArray($path, $value));
+            }
+            $this->output->writeln(json_encode($rows));
         }
+    }
+
+    private function pathAndFieldResolver($field): array
+    {
+        $parts = array_filter(array_map('trim', explode(":", $field)));
+        $path = $parts[0];
+        // Use path as the resolver as it is shorthanded
+        $resolver = $parts[1] ?? $path;
+
+        // the resolver is like a property call
+        if (false === strpos($resolver, '(')) {
+            return [$path, $resolver, []];
+        }
+
+        // the resolver is the method
+        $expression = "/\s*(\w+)\s*\((.*)\)/";
+        preg_match($expression, $resolver, $extraction);
+        $method = $extraction[1];
+        $arguments = $extraction[2];
+
+        /**
+         * I Know what I am doing
+         * Converting the method parameters in an array to pass
+         * it on on the next faker call
+         */
+        eval("\$arguments = [{$arguments}];");
+
+        return [$path, $method, $arguments];
+    }
+
+    private function dotNotationToArray($path, $value)
+    {
+        return array_reduce(array_reverse(array_filter(array_map('trim', explode('.', $path)))),
+            function ($previous, $current) {
+                return [$current => $previous];
+            }, $value);
     }
 
     private function getNoOfEntries(): int
@@ -93,5 +167,10 @@ class GenerateCommand extends Command
         $docStartID = (int)$this->option('id');
 
         return $docStartID >= 0 ? $docStartID : 1;
+    }
+
+    private function parseFields(): array
+    {
+        return array_filter(array_map('trim', explode('|', $this->fields)));
     }
 }
